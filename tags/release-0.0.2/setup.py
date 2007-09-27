bf80b2f46688e9213a587c6ac73579ed232b53df
AlenLPeacock <AlenLPeacock@095d5ecb-1529-0410-9ab4-bbfc146bbd73>
2007-09-27 06:06:58 +0000

    tagging the 0.0.2 release of flud backup

---
+++ tags/release-0.0.2/setup.py
@@ -0,0 +1,68 @@
+from distutils.core import setup, Extension
+import os, sys
+
+class AutoToolsExtension(Extension):
+	'''
+	provides simple hooks for running ./configure, make, and any pre-configure
+	scripts (such as ./bootstrap) necessary to build an Extension package.
+	'''
+	def __init__(self, name, sources=[], extra_objects=[], include_dirs=[], 
+			libraries=[], library_dirs=[], language=[], pre_configure=[],
+			run_configure=[], run_make=[]):
+		for dir, cmd in pre_configure:
+			r = os.system('cd %s; %s' % (dir, cmd))
+			if r != 0: sys.exit()
+		for dir in run_configure:
+			r = os.system('cd %s; ./configure' % dir)
+			if r != 0: sys.exit()
+		for dir in run_make:
+			r = os.system('cd %s; make' % dir)
+			if r != 0: sys.exit()
+		Extension.__init__(self, name, sources=sources,
+				extra_objects=extra_objects, include_dirs=include_dirs,
+				libraries=libraries, library_dirs=library_dirs,
+				language=language)
+
+setup(name="flud",
+		version="0.0.2", 
+		description="flud decentralized backup", 
+		long_description='a 100% decentralized backup system',
+		author="Alen Peacock",
+		author_email="apeacock@flud.org",
+		url='http://flud.org',
+		copyright='(c)2004-2007 Alen Peacock, licensed GPL v3',
+		license='GPL v3',
+		packages=['flud', 
+			'flud.protocol', 
+			'flud.bin', 
+			'flud.test'],
+		package_dir={'flud': 'flud', 
+			'flud.protocol': 'flud/protocol',
+			'flud.bin': 'flud/bin',
+			'flud.test': 'flud/test'},
+		package_data={'flud': ['images/*.png', 'FludNode.tac']},
+		scripts = ['flud/bin/fludnode',
+			'flud/bin/tacpath-flud',
+			'flud/bin/fludscheduler', 
+			'flud/bin/fludclient', 
+			'flud/bin/fludlocalclient',
+			'flud/bin/flud-mastermetadataViewer',
+			'flud/bin/flud-metadataViewer',
+			'flud/bin/start-fludnodes', 
+			'flud/bin/stop-fludnodes', 
+			'flud/bin/gauges-fludnodes',
+			'flud/bin/clean-fludnodes'],
+		ext_modules=[AutoToolsExtension('filecoder',
+			pre_configure = [('flud/coding', './bootstrap')],
+			run_configure = ['flud/coding'],
+			run_make = ['flud/coding/ldpc', 'flud/coding'],
+			sources = ['flud/coding/filecodermodule.cpp'],
+			extra_objects = ['flud/coding/CodedBlocks.o', 'flud/coding/Coder.o',
+				'flud/coding/Decoder.o'],
+			include_dirs = ['flud/coding/ldpc/src', 'flud/coding'],
+			libraries = ['ldpc', 'stdc++'],
+			library_dirs = ['flud/coding/ldpc/bin/linux'],
+			language = ['c++'])]
+		)
+
+
